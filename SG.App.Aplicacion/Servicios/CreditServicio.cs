ecd66c7a1e39dccec79b0f4a813ae6a96a39e6fa
AndresSimba <wilo.andres14@gmail.com>
2024-06-25 23:38:46 -0500

    Interfaz - Implementacion - Servicio Tabla CREDIT

---
+++ SG.App.Aplicacion/Servicios/CreditServicio.cs
@@ -0,0 +1,71 @@
+﻿using SG.Dominio.Modelo.Abstracciones;
+using SG.Dominio.Modelo.Entidades;
+using SG.Infraestructura.AccesoDatos.Repositorio.Implementaciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG.App.Aplicacion.Servicios
+{
+    public class CreditServicio
+    {
+        readonly ICreditRepositorio creditServicio;
+
+        public CreditServicio()
+        {
+            creditServicio = new CreditRepositorioImpl();
+        }
+
+        public void InsertarCredit(credit credit)
+        {
+            try
+            {
+                creditServicio.Add(credit);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error Servicio: No se pudo insertar registro CREDIT,", ex);
+            }
+        }
+
+        public void ModificarCredit(credit credit)
+        {
+            try
+            {
+                creditServicio.Modify(credit);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Servicio: No se pudo actualizar el registro CREDIT,", ex);
+            }
+        }
+
+        public void EliminarCredit(int id)
+        {
+            try
+            {
+                creditServicio.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Servicio: No se pudo eliminar el registro CREDIT,", ex);
+            }
+        }
+
+        public IEnumerable<credit> listarTodo()
+        {
+            try
+            {
+                return creditServicio.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Servicio: No se pudo traer los registros CREDIT,", ex);
+            }
+        }
+    }
+}
+

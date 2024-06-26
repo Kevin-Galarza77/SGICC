a75da6ec73e89bf014bcc5939f2407386d5f59e3
AndresSimba <wilo.andres14@gmail.com>
2024-06-25 22:36:28 -0500

    Implementacion Usuario Completo

--- SG.App.Aplicacion/Servicios/UsuarioServicio.cs
+++ SG.App.Aplicacion/Servicios/UsuarioServicio.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SG.Dominio.Modelo.Abstracciones;
+using SG.Dominio.Modelo.Entidades;
+using SG.Infraestructura.AccesoDatos.Repositorio.Implementaciones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,65 @@ using System.Threading.Tasks;
 
 namespace SG.App.Aplicacion.Servicios
 {
-    public class UsuarioServicio
+    public class UsuarioServicio 
     {
+        readonly IUsuariosRepositorio usuariosRepositorio;
+
+        public UsuarioServicio()
+        {
+            usuariosRepositorio = new UsuarioRepositorioImpl();
+                
+        }
+
+        public void InsertarUsuario(users users)
+        {
+            try
+            {
+                usuariosRepositorio.Add(users);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error Servicio: No se pudo insertar registro USUARIO,", ex);
+            }
+        }
+
+
+        public void ModificarUsuario(users users)
+        {
+            try
+            {
+                usuariosRepositorio.Modify(users);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Servicio: No se pudo actualizar el registro USUARIO,", ex);
+            }
+        }
+
+        public void EliminarUsuario(int id)
+        {
+            try
+            {
+                usuariosRepositorio.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Servicio: No se pudo eliminar el registro USUARIO,", ex);
+            }
+        }
+
+        public IEnumerable<users> listarTodo()
+        {
+            try
+            {
+                return usuariosRepositorio.GetAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error Servicio: No se pudo traer los registros USUARIO,", ex);
+            }
+        }
+
     }
 }
